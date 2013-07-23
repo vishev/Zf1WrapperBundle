11ce9b1531c621033f76fc942710c5ba4dabe4bf
Sander van Thillo <othillo@othillo.nl>
2013-07-23 14:06:36 +0200

    pass Container to Zend_Registry, removed logic for Location header

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace MainlyCode\Zf1WrapperBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Zend_Registry;
 
 class DefaultController extends Controller
 {
@@ -16,22 +17,16 @@ class DefaultController extends Controller
      */
     public function indexAction($url)
     {
-        $requestUri = $this->getRequest()->getRequestUri();
-        //var_dump($requestUri);
+        // pass Dependency Injection Container
+        Zend_Registry::set('dic', $this->container);
 
-        // capture content from legacy application
         $rootDir   = $this->get('kernel')->getRootDir();
         $bootstrap = $this->container->getParameter('zf1wrapper_bootstrap_path');
 
+        // capture content from legacy application
         ob_start();
         include $rootDir . '/' . $bootstrap;
         $content = ob_get_clean();
-
-        // exit on Location header
-        $matches = array_filter(headers_list(), function ($v) { return 'Location:' == substr($v, 0, 9); });
-        if (count($matches) >0) {
-            exit;
-        }
 
         return new Response($content);
     }
